a2d02c9d0146a9ae4e0469014f0e24c007dbcdfa
Sharvari Thakar <Sharvari.Thakar@green-apex.com>
2021-06-29 12:50:43 +0530

    linkedin response change

--- src/app/linked-in-response/linked-in-response.component.ts
+++ src/app/linked-in-response/linked-in-response.component.ts
@@ -15,16 +15,7 @@ export class LinkedInResponseComponent implements OnInit {
 
   ngOnInit() {
     this.linkedInToken = this.route.snapshot.queryParams["code"];
-    var body = {
-      grant_type: "authorization_code",  // value of this field should always be: 'authorization_code'
-      code: this.linkedInToken,
-      redirect_uri: "http://localhost:4200/linkedIn",  // The same redirect_url used in step 2.1 (in login.component.ts)
-      client_id: '86jyakbedizz0r', // Follow step 1.2
-      client_secret: 'KFCGhreTS3rXiDqN'   // Follow step 1.2
-    }
-    this.http.get('https://www.linkedin.com/oauth/v2/accessToken').subscribe(res => {
-      console.log(res)
-    })
+    console.log("LinkedIn ---", this.linkedInToken)
   }
 
 }
